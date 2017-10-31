93049bccba1a44271842ebd0acc5acc813335f39
Michael Spector <spektom@gmail.com>
2017-10-31 16:34:52 +0200

    Try to optimize TravisCI build process by using Docker devenv image

--- devenv/docker/prepare.sh
+++ devenv/docker/prepare.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t viya-devenv .
+docker build -t viyadb/devenv:latest .
--- devenv/docker/run.sh
+++ devenv/docker/run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker ps -a | grep viya-devenv | awk '{print $1}' | xargs docker rm
-docker run --security-opt seccomp=unconfined -v $(pwd)/../../:/viyadb -p 5000:5000 -ti viya-devenv "$@"
+exec docker run --rm --security-opt seccomp=unconfined -v $(pwd)/../../:/viyadb -p 5000:5000 -ti viyadb/devenv:latest "$@"
