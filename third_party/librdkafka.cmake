cd8a3afdba27c7b1835609823fbd8fe306595d5f
Michael Spector <spektom@gmail.com>
2017-12-04 00:08:41 +0200

    Fixed Docker image

--- third_party/librdkafka.cmake
+++ third_party/librdkafka.cmake
@@ -11,3 +11,5 @@ execute_process(COMMAND make libs
 execute_process(COMMAND make install
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/librdkafka
 )
+
+install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/librdkafka/lib/ DESTINATION lib FILES_MATCHING PATTERN "*.so*")
